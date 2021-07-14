ca1ef6c1260a0f13ba00708a361656eb6a3afe20
Bulat-l <bulatsharipov240@gmail.com>
2021-07-14 21:15:18 +0300

    one more try

--- tasks.py
+++ tasks.py
@@ -1,7 +1,8 @@
 from app import create_app
 import os
 
+
+app = create_app()
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 320029))
-    app = create_app()
     app.run(debug=False, port=port)
