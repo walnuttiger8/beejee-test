d9700c2dfcbfb5cd123cd5414e7419785fb9314b
Bulat-l <bulatsharipov240@gmail.com>
2021-07-14 20:10:53 +0300

    deploy port 2

--- tasks.py
+++ tasks.py
@@ -2,6 +2,6 @@ from app import create_app
 import os
 
 
-port = int(os.environ.get('PORT', 33507))
+port = int(os.environ.get('PORT', 320029))
 app = create_app()
-app.run(debug=False)
+app.run(debug=False, port=port)
